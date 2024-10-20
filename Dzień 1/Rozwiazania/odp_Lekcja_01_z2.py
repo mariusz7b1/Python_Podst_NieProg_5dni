ae9939132466e093a47da187d424cc24174471e5
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2024-10-20 17:23:34 +0200

    czyszczenie

--- Dzień 1/Rozwiazania/odp_Lekcja_01_z2.py
+++
@@ -1,3 +0,0 @@
-print()
-print('Twoja ksywa to\n\t"Tiger"\n', "znaczy sie 'Tygrys'")
-input()
--- Dzień 2/Przyklady/while/02_while_pomidor.py
+++
@@ -1,9 +0,0 @@
-""" implementacja ppopularnej w latach 70-80 :) gry pomidor
-    w erze przed smartfonowej
-"""
-from os import system
-system('cls')
-
-slowo = 'pomidor'
-while slowo == 'pomidor':
-    slowo = input('podaj slowo: ')
