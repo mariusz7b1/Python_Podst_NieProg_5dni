578afa0201d15fc5f8f0eda9e48f97b2ebfc6cfc
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2024-10-20 17:24:06 +0200

    czyszczenie1

--- Dzien 4/Przyklady/Wyjatki/05_error_raise.py
+++
@@ -1,6 +0,0 @@
-try:
-    age = int(input("Podaj swój wiek: "))
-    if age < 0:
-        raise ValueError("Wiek nie może być ujemny")
-except ValueError as e:
-    print(e)
--- Dzien 4/Przyklady/moduly_wbudowane/02_import.py
+++
@@ -1,2 +0,0 @@
-import math as m
-print(m.sin(m.pi*0.5))
