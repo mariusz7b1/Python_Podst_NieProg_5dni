cc178bf953efb01ebe3c0fae9c23ba6431ddedcf
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2024-12-17 08:34:38 +0100

    start2

---
+++ Dzien 2/Przyklady/while/03_while_else_break.py
@@ -0,0 +1,14 @@
+""" 
+while - else break 
+"""
+from os import system
+system('cls')
+
+war = 0
+while war != 10:    # rożne od 10
+    war = int(input('Podaj liczbę (10-ZAKONCZENIE) '))
+    if war == 666:
+        print('!! ZLE !!')
+        break
+else:
+    print('Zakonczone z sukcesem')
