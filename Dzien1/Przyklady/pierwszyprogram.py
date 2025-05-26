94fd327f450c64a45b81362cda33797798de3bf7
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2025-05-26 11:53:30 +0200

    update1

---
+++ Dzien1/Przyklady/pierwszyprogram.py
@@ -0,0 +1,9 @@
+print("Czesć jestem Python")
+imie = input("A jak ty się nazywasz ? ")
+print("witaj",imie)
+# wiek = int(input("Ile masz lat ? "))
+wiek = input("Ile masz lat ? ")
+wiek_int = int(wiek)
+
+do_setki = 100-wiek_int
+print("Do setki brakuje ci "+str(do_setki)+" lata")
